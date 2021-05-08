2e7786523e740d971ee9f691520d8a6defd4cb09
Elena Sviridova <valeryk13@yandex.ru>
2021-05-08 19:36:25 +0300

    10sprint thrd

--- src/components/App.js
+++ src/components/App.js
@@ -10,13 +10,13 @@ function App() {
     const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
     const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false);
     const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
-    const [selectedCard, setSelectedCard] = useState('');
+    const [selectedCard, setSelectedCard] = useState(null);
 
     function closeAllPopups() {
         setIsEditAvatarPopupOpen(false);
         setIsEditProfilePopupOpen(false);
         setIsAddPlacePopupOpen(false);
-        setSelectedCard('')
+        setSelectedCard(null)
     }
 
     function handleEditAvatarClick() {
